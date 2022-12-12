841b18cb42611b578b1b2a70dc0211558e100e7d
NoahMulu <noahmulu1993@gmail.com>
2022-12-12 01:40:03 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry
- *
- * Return: Always 0 (success)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-
 int main(void)
 {
         puts("\"programming is like bulding multilnigiual puzzle");
